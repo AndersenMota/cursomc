8f676e365bdf7504b9ef10881dd4f313d55f705d
Andersen Mota <andersenmota.2@gmail.com>
2023-06-27 09:58:04 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/com/andersenmota/cursomc/services/CategoriaService.java
@@ -0,0 +1,18 @@
+package com.andersenmota.cursomc.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.andersenmota.cursomc.domain.Categoria;
+import com.andersenmota.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
