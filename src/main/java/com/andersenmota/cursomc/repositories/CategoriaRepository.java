7f35e240057995317fe57cf38f049c8b8df847e8
Andersen Mota <andersenmota.2@gmail.com>
2023-06-30 13:59:55 -0400

    Produto e associacao muitos para muitos

--- src/main/java/com/andersenmota/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/andersenmota/cursomc/repositories/CategoriaRepository.java
@@ -1,10 +1,10 @@
 package com.andersenmota.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import com.andersenmota.cursomc.domain.Categoria;
 
-@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+
 	
 }
--- src/main/java/com/andersenmota/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andersenmota/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,10 @@
 package com.andersenmota.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.andersenmota.cursomc.domain.Categoria;
 import com.andersenmota.cursomc.services.CategoriaService;
@@ -18,7 +15,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	@GetMapping("/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		
