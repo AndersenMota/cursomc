078b1b8abea250ca26374c31820e7c823eab4cea
Andersen Mota <andersenmota.2@gmail.com>
2023-07-06 16:13:39 -0400

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/andersenmota/cursomc/services/CategoriaService.java
+++ src/main/java/com/andersenmota/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.andersenmota.cursomc.domain.Categoria;
 import com.andersenmota.cursomc.repositories.CategoriaRepository;
+import com.andersenmota.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,6 +14,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 }
